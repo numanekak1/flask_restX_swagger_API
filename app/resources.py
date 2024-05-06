6aa10b445d7e08b2473006b0f24ffb5c6402dc72
Mohd Numan <numan@ekak.in>
2024-05-06 18:08:21 +0530

    Create DB And EndPoint Get Request

--- app/resources.py
+++ app/resources.py
@@ -1,5 +1,8 @@
 from flask_restx import Resource, Namespace
 
+# Local Modules
+from .models import Course,Student
+from .api_models import course_model,student_model
 ns= Namespace(name="Basic api",path='/api/v1/')
 
 @ns.route('/hello')
@@ -8,3 +11,18 @@ class Hello(Resource):
         return {"status":"get"}
     def post(self):
         return {"status":"post"}
+    
+
+@ns.route('/courses')
+class CourseAPI(Resource):
+    @ns.marshal_list_with(course_model) #taking output and return serialised way
+    def get(self):
+        return Course.query.all()
+    
+
+@ns.route('/students')
+class StudentAPI(Resource):
+    @ns.marshal_list_with(student_model)
+    def get(self):
+        return Student.query.all()
+    
